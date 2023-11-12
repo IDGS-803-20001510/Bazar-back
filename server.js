930876875bf775815a5a0683a9f1be7ed1f3836e
IDGS-803-20001510 <alexis_prado_ojeda@hotmail.com>
2023-11-12 13:43:06 -0600

    cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const itemsRouter = require('./back/routes/items');
 
+app.use(cors());
+
 app.use('/api/items', itemsRouter);
 
-const PORT = process.env.PORT || 3001; 
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
